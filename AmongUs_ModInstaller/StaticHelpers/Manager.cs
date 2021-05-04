291cf1b4644a7bad390e24e387ef751bb3ac749a
NyphoX <nyphox@nyphox.ovh>
2021-05-04 03:16:58 +0200

    Fixed exception where stored settings were not overwriting files correctly.

--- AmongUs_ModInstaller/StaticHelpers/Manager.cs
+++ AmongUs_ModInstaller/StaticHelpers/Manager.cs
@@ -186,7 +186,7 @@ namespace AmongUs_ModInstaller
                 string tempFile = tuple.Item2;
                 string combinedPath = Path.Combine(modInstallation.absolutePath, configFileSubPath);
                 Directory.CreateDirectory(Path.GetDirectoryName(combinedPath));
-                File.Copy(tempFile, Path.Combine(combinedPath));
+                File.Copy(tempFile, Path.Combine(combinedPath), true);
                 File.Delete(tempFile);
             }
         }
